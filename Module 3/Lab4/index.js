b0f2beddbb9f67a7cd0eaa7041fcf7e23af2e744
Mario Valencia <vmmario@gmail.com>
2017-08-12 12:56:22 -0500

    Agosto 12

---
+++ Module 3/Lab4/index.js
@@ -0,0 +1,10 @@
+var a = "variable";
+var b = 5;
+var c = true;
+var d = ['a', 'b', 'c'];
+var e;
+console.log("a = " + a);
+console.log("b = " + b);
+console.log("c = " + c);
+console.log("d = " + d);
+console.log("e = " + e);
---
+++ Module 3/Lab4/index.ts
@@ -0,0 +1,13 @@
+
+
+let a : String = "variable";
+let b : number = 5;
+let c : boolean = true;
+let d : Array<String> = ['a', 'b', 'c'];
+let e : any[];
+
+console.log("a = " + a);
+console.log("b = " + b);
+console.log("c = " + c);
+console.log("d = " + d);
+console.log("e = " + e);
